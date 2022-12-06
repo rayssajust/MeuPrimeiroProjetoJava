1b369ed48f96d519a2b691a3048195b0a096f8a8
Rayssa <ray.justino15@gmail.com>
2022-12-06 17:09:08 -0300

    MeuPrimeiroCommit

---
+++ src/meuprimeiroprojetojava/MeuPrimeiroProjetoJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meuprimeiroprojetojava;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rayssa-PC
+ */
+public class MeuPrimeiroProjetoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "olá, mundo!");
+         }
+    
+}
